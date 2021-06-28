afff7939b42926c32a7a188e5c448fc241b74c71
Kim-J-Y <kjykjykjy500@naver.com>
2021-06-28 15:59:31 +0900

    SpringBoot 설정
    Boot초기 설정(pom.xml -dependency, application.properties, tiles 예제 파일,
    mybatis xml 템플릿, homecontroller - home.jsp
    
    Security는 dependency만 추가, 주석처리되있음

---
+++ Pamuk/src/main/java/org/kosta/pamuk/PamukApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.pamuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamukApplication.class, args);
+	}
+
+}
---
+++ Pamuk/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hi-에이치 아이</title>
+</head>
+<body>
+dfd
+</body>
+</html>
