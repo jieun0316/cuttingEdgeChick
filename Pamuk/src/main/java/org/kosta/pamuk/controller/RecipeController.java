b0a8c54976b647faba5fd241b9c1ea5a8643d73f
Chenjae <injea95@naver.com>
2021-06-30 16:33:06 +0900

    Recipe

--- Pamuk/src/main/java/org/kosta/pamuk/controller/RecipeController.java
+++ Pamuk/src/main/java/org/kosta/pamuk/controller/RecipeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 
 /**
  * Recipe 게시판에 관련된 비즈니스 로직을 정의합니다
- * @author 최인재
+ * 
  *
  */
 @Controller
---
+++ Pamuk/src/main/java/org/kosta/pamuk/service/RecipeService.java
@@ -0,0 +1,12 @@
+package org.kosta.pamuk.service;
+
+import org.springframework.stereotype.Service;
+/**
+ * 
+ * @author 최인재
+ *
+ */
+@Service
+public class RecipeService {
+
+}
