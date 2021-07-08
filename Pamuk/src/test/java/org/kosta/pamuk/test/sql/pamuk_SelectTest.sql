af74d739481b50ba7ebc3edd31e30b012c540e4e
suisuiee <binnee7@hanmail.net>
2021-07-08 16:10:58 +0900

    충돌 해결

--- Pamuk/src/test/java/org/kosta/pamuk/test/sql/pamuk_SelectTest.sql
+++ Pamuk/src/test/java/org/kosta/pamuk/test/sql/pamuk_SelectTest.sql
@@ -4,7 +4,6 @@ select * from RECIPE_ITEM;
 select * from RECIPE_CONTENT;
 select * from authorities;
 
-
 SELECT M.name, M.nick, A.authority FROM member M, authorities A	WHERE M.member_id=A.member_id;
 select count(*) from member where member_id='aa'
 
